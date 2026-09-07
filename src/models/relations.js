6d1207a92dacd4402fe7b80e3571c5eabc5665a1
WinG-E00 <gastonencinawj@gmail.com>
2026-09-07 17:51:14 -0300

    proyecto-integrador: Configuracion de relaciones

---
+++ src/models/relations.js
@@ -0,0 +1,55 @@
+import User from "./user.model";
+import Profile from "./profile.model";
+import Article from "./article.model";
+import ArticleTag from "./articleTag.model";
+
+//Relacion de uno a uno con profile
+// User tiene un profile 
+User.hasOne(Profile, {
+  foreignKey: 'user_id',
+  as: 'profile'
+})
+
+Profile.belongsTo(User, {
+  foreignKey: 'user_id',
+  as: 'user'
+})
+
+
+
+
+
+//relacion de muchos 
+User.hasMany(Article, {
+  foreignKey: 'article_id',
+  as: 'article'
+})
+
+Article.belongsTo(User, {
+  foreignKey: 'article_id',
+  as: 'author'
+})
+
+
+
+
+//relacion de muchos a muchos
+Article.belongsToMany(Tag, {
+  through: ArticleTag,
+  foreignKey: 'article_id',
+  otherKey: 'tag_id',
+  as: 'tags',
+});
+
+Tag.belongsToMany(Article, {
+  through: ArticleTag,
+  foreignKey: 'tag_id',
+  otherKey: 'article_id',
+  as: 'articles',
+});
+
+
+
+
+
+
